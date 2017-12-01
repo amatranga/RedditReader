86f191a4c8adab2684cfdd48a7e5e0bd87eb32e4
amatranga <amatranga892@gmail.com>
2017-11-30 22:48:54 -0600

    Update tests and install needed dependencies
    There is an error somewhere in the environment...

---
+++ client/test/fixtures/postFixture.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {posts: 
+    {
+      data: {
+        author: 'Test User',
+        permalink: 'https://www.reddit.com/r/all/',
+        subreddit_name_prefixed: 'r/all',
+        title: 'A test post',
+        url: 'www.example.com'
+      }
+    }
+  }
+];
